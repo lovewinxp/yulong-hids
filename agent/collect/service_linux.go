54b53f280310943e95b4266b2ddb5cc4502fec86
zj1244 <zj1244@qq.com>
2018-12-03 16:01:09 +0800

    修改centos7下获取服务问题

--- agent/collect/service_linux.go
+++ agent/collect/service_linux.go
@@ -21,10 +21,47 @@ type service struct {
 func GetServiceInfo()  (resultdata []map[string]string) {
 
 	var serviceStr string=""
-	//serviceStr := common.Cmdexec("systemctl")
-	serviceStr = common.Cmdexec("chkconfig --list")
+	serviceStr = common.Cmdexec("systemctl list-units --type service")
+	//serviceStr = common.Cmdexec("chkconfig --list")
 	if serviceStr != "" {
 
+		serviceList := strings.Split(serviceStr, "\n")
+		if len(serviceList) < 2 {
+			return
+		}
+		for _, info := range serviceList[1 : len(serviceList)-1] {
+
+			if !strings.Contains(info,"service") {
+				continue
+			}
+			m := make(map[string]string)
+			reg := regexp.MustCompile("\\s+")
+			info = reg.ReplaceAllString(strings.TrimSpace(strings.Replace(info,"●","",-1)), " ")
+			s := strings.Split(info, " ")
+			if len(s) < 4 {
+				continue
+			}
+			m["name"] = s[0]
+			//"pathname": "启动命令，同command",
+			//	"started": "当前启动状态",
+			//	"startmode": "开机启动模式",
+			//	"startname": "启动用户",
+			//	"caption": "描述"
+			//m["pathname"] = "pathname"
+			m["started"] = s[3]
+
+			m["caption"] = string([]rune(info)[strings.Index(info,s[4]):])
+
+
+			resultdata = append(resultdata, m)
+
+		}
+
+
+
+
+	}	else 	{
+		serviceStr = common.Cmdexec("chkconfig --list")
 		serviceList := strings.Split(serviceStr, "\n")
 		if len(serviceList) < 2 {
 			return
@@ -54,12 +91,6 @@ func GetServiceInfo()  (resultdata []map[string]string) {
 			resultdata = append(resultdata, m)
 
 		}
-
-
-
-
-	}	else 	{
-		serviceStr = common.Cmdexec("chkconfig --list")
 
 	}
 
